de824bb8ab8bce66cdd046e8ae77a3b1b6d675fc
Vozudo <jploperatobon@gmail.com>
2022-09-16 12:24:47 -0500

    return description

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,6 +2,7 @@
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node
+ * Return: height or NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {

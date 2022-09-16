adb006d5fda37297fd5830f31e6aba5786ba5605
Vozudo <jploperatobon@gmail.com>
2022-09-16 12:30:47 -0500

    Depht of the tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node
- * Return: height or NULL
+ * Return: height or 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {

9b4b4a0950753b9e6feea33aa163a3a321f6fb28
Vozudo <jploperatobon@gmail.com>
2022-11-18 07:01:25 -0500

    Betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,11 +6,11 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
-        return (1);
-    return (0);
+	if (!tree)
+		return (0);
+	if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
+		return (1);
+	return (0);
 }
 
 /**

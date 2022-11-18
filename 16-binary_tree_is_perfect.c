3ef9c572bfba666d1a937565e9ed25281a3b3101
Vozudo <jploperatobon@gmail.com>
2022-11-18 07:00:03 -0500

    [task 16] is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,5 +6,27 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+    if (!tree)
+        return (0);
+    if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
+        return (1);
+    return (0);
+}
 
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: size of the tree or 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t s = 0;
+
+	if (tree)
+	{
+		s += 1;
+		s += binary_tree_size(tree->left);
+		s += binary_tree_size(tree->right);
+	}
+	return (s);
 }
